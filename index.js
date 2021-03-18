2d3879c8d36a75e338605fafba8473c6ce5be637
ziitania <tjhaweszii@gmail.com>
2021-03-18 13:16:45 -0700

    commit tree 48e6da17bcd180cdef337ae6c14fc98c3e9139f1

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(drivArra) {
+    const newArra = drivArra.slice(0, 2);
+    return newArra;
+};
+
+const returnLastTwoDrivers = function(drivArra) {
+    const newArra = drivArra.slice(drivArra.length - 2);
+    return newArra;
+};
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = function(fareMult){
+    return function(fare) {
+        return fare * fareMult;
+    };
+};
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(drivArra, resultFunct) {
+    return resultFunct(drivArra);
+}
